bd2bf053ac922e9ea4ac85acd725cceacb51d6a1
Alex S <124331980+AlexSip@users.noreply.github.com>
2023-03-17 23:10:11 -0400

    Delete Sip_Coding_Challenge2-main directory

--- Sip_Coding_Challenge2-main/Task1.py
+++
@@ -1,3 +0,0 @@
-numList = [1, 2, 3, 6, 8, 12, 20, 32, 46, 85]
-newList = [i for i in numList if i <= 5]
-print(newList)
--- Sip_Coding_Challenge2-main/Task4.py
+++
@@ -1,4 +0,0 @@
-age = int(input("What is your age? "))
-print ("Your age is " + str(age))
-i = 65 - age
-print(f"You have {i} years until you retire")
